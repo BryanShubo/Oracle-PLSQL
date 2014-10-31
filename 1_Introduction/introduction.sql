21659acc970ce4813ad3fcbd66296666315cb311
Bryan <bryanshubo@gmail.com>
2014-10-30 22:11:17 -0400

    introduction.sql

---
+++ 1_Introduction/introduction.sql
@@ -0,0 +1,14 @@
+--1.1.dual	
+--1.2.Arithmetic operator	
+--1.3.Comparison Operators	
+--1.4.Logical Operators	
+--1.5.Null Values	
+--1.6.Print
+--1.7.Variable
+--1.8.Comments
+--1.9.Terminology
+--1.10.Database Normalization
+
+--1.1 dual
+
+
