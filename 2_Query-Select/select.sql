e4efeec902b15f9efabee6a379482112c5bb980a
Bryan <bryanshubo@gmail.com>
2014-11-03 08:39:26 -0500

    create select .sql

---
+++ 2_Query-Select/select.sql
@@ -0,0 +1,70 @@
+2.1.Introduction	
+2.2.Select clause	
+2.3.Where	
+2.4.Order By	
+2.5.Group By	
+2.6.HAVING	
+2.7.DISTINCT	
+2.8.Columns	
+2.9.Concatenation	
+2.10.Arithmetic	
+2.11.AND OR	
+2.12.Alias	
+2.13.ALL	
+2.14.ANY	
+2.15.CASE	
+2.16.BETWEEN	
+2.17.IN	
+2.18.EXISTS	
+2.19.LIKE	
+2.20.IS INFINITE	
+2.21.IS NAN	
+2.22.IS NOT INFINITE
+2.23.IS NOT NAN
+2.24.IS NULL
+2.25.NOT
+2.26.NOT BETWEEN
+2.27.NOT IN
+2.28.NOT LIKE
+2.29.Level
+2.30.ROWID
+2.31.rownum
+2.32.GUID
+2.33.ROW
+2.34.START WITH
+2.35.with
+2.36.Single Row Subquery
+2.37.Correlated Subquery
+2.38.Multiple Row Subquery
+2.39.Nested Subquery
+2.40.Hierarchical Queries
+2.41.Multi column subquery
+2.42.CONNECT
+
+
+2.1 Introduction
+The SQL operators allow you to limit rows based on
+*pattern matching of strings,
+*lists of values,
+*ranges of values, and
+*null values.
+
+The SQL operators are listed in the following table:
+Operator	Description
+*LIKE	Matches patterns in strings
+*IN	Matches lists of values
+*BETWEEN	Matches a range of values
+*IS NULL	Matches null values
+*IS NAN	New for Oracle10g. Matches the NaN special value, which means "not a number"
+*IS INFINITE	New for Oracle10g. Matches infinite BINARY_FLOAT and BINARY_DOUBLE values
+
+You can also use the NOT operator to reverse the meaning of LIKE, IN, BETWEEN, and IS NULL:
+*NOT LIKE
+*NOT IN
+*NOT BETWEEN
+*IS NOT NULL
+*IS NOT NAN
+*IS NOT INFINITE
+
+
+2.2 Select clause
